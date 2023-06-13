a73867c38d4fe950b5db3119e2e3e10e5b7d45c5
Sunitha <136448809+SunithaBisinigi@users.noreply.github.com>
2023-06-13 16:04:38 +0530

    Using DOM

---
+++ javascript.js
@@ -0,0 +1,22 @@
+let box=document.getElementsByClassName("box");
+let a=document.getElementById("blue");
+a.onclick=function(){
+    box[0].style.backgroundColor="blue";
+};
+
+let b=document.getElementById("red");
+b.onclick=function(){
+    box[0].style.backgroundColor="red";
+};
+
+let g=document.getElementById("green");
+g.onclick=function(){
+    box[0].style.backgroundColor="green";
+};
+
+let p=document.getElementById("purple");
+p.onclick=function(){
+    box[0].style.backgroundColor="purple";
+};
+
+
---
+++ script.js
@@ -0,0 +1,6 @@
+let box=document.getElementsByClassName("box");
+let a=document.getElementById("blue");
+a.onclick=function(){
+    box[0].style.backgroundColor="blue";
+};
+
